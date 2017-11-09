365a095789a6d7b39ee7761ea75d8077488910c5
Hatem <hatem@lanterncreative.ca>
2017-11-09 14:03:52 -0800

    Add files via upload
    check if proper files are actually in /test

---
+++ olympus.js
@@ -0,0 +1,16 @@
+// var styles = {
+//   color: "red",
+//   background: "pink",
+//   border: "2px sold purple"
+// }
+
+// $("h1").css(styles)
+
+// $("button").on("mouseenter", function(){
+//   $(this).css("font-weight", "bold");
+// });  
+
+$(".begin").hide();
+$("#begin").hide().fadeIn(3000, function() {
+    $(".begin").fadeIn(2000);
+});  
